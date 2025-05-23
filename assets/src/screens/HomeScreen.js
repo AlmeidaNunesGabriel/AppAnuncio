2b5adde6205e4bb53c9548e6d20c82779215e6ce
AlmeidaNunesGabriel <dev.almeidanunesgabriel@gmail.com>
2025-05-23 17:47:44 -0300

    eas update

--- assets/src/screens/HomeScreen.js
+++ assets/src/screens/HomeScreen.js
@@ -3,10 +3,10 @@ import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import ProductCard from '../components/ProductCard';
 
 const produtos = [
-  { id: 1, titulo: 'Tênis Esportivo', foto: 'https://via.placeholder.com/150/FF6347', descricao: 'Tênis confortável para corrida.' },
-  { id: 2, titulo: 'Camisa Casual', foto: 'https://via.placeholder.com/150/4682B4', descricao: 'Camisa estilosa para o dia a dia.' },
-  { id: 3, titulo: 'Mochila Escolar', foto: 'https://via.placeholder.com/150/32CD32', descricao: 'Perfeita para levar seus materiais.' },
-  { id: 4, titulo: 'Relógio Digital', foto: 'https://via.placeholder.com/150/FFD700', descricao: 'Tecnologia e estilo no seu pulso.' },
+  { id: 1, titulo: 'Tênis Esportivo', foto: 'https://placehold.co/600x400', descricao: 'Tênis confortável para corrida.' },
+  { id: 2, titulo: 'Camisa Casual', foto: 'https://placehold.co/600x400', descricao: 'Camisa estilosa para o dia a dia.' },
+  { id: 3, titulo: 'Mochila Escolar', foto: 'https://placehold.co/600x400', descricao: 'Perfeita para levar seus materiais.' },
+  { id: 4, titulo: 'Relógio Digital', foto: 'https://placehold.co/600x400', descricao: 'Tecnologia e estilo no seu pulso.' },
 ];
 
 export default function HomeScreen({ navigation }) {
